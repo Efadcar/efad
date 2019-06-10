78776a10130fe823d44e1d49cffb48f748e0f776
Ebrahim Elsawy <elsawy@asait.co>
2019-06-11 00:41:08 +0200

    edit autoload

--- admin/application/config/autoload.php
+++ admin/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','messages','instagram_api');
+$autoload['libraries'] = array('database','session','messages');
 
 /*
 | -------------------------------------------------------------------
