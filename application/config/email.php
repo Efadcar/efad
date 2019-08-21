1e7284d4085f19541bf2d8991dfda9a4a73aafe5
Ebrahim Elsawy <elsawy@asait.co>
2019-08-22 00:20:28 +0200

    booking email working

--- application/config/email.php
+++ application/config/email.php
@@ -1,2 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Mailgun Configuration
+ */
+$config['smtp_crypto'] = 'tls'; 
+$config['protocol']         = 'smtp';
+$config['smtp_host']        = 'smtp.office365.com';
+$config['smtp_port'] 		= '587';
+$config['smtp_user']        = 'wecare@efadcar.com';
+$config['smtp_pass']        = 'Wecare123456%';
+$config['charset']          = 'utf-8';
+$config['mailtype']         = 'html';
+$config['priority']         = 3;
+$config['smtp_timeout']		= 1;
+$config['newline']="\r\n"; //"\r\n" or "\n" or "\r". DEFAULT should be "\r\n" 
+$config['crlf'] = "\r\n"; //"\r\n" or "\n" or "\r" DEFAULT should be "\r\n" 
