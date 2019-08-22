2fed48b1b91a729347f169cd8f278741cacbb847
Ebrahim Elsawy <elsawy@asait.co>
2019-08-22 15:31:24 +0200

    change mail config

--- application/config/email.php
+++ application/config/email.php
@@ -4,15 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * Mailgun Configuration
  */
-$config['smtp_crypto']		= 'tls'; 
+//$config['smtp_crypto']		= 'tls'; 
 $config['protocol']         = 'smtp';
-$config['smtp_host']        = 'smtp.office365.com';
+$config['smtp_host']        = 'localhost';
 $config['smtp_port'] 		= '587';
-$config['smtp_user']        = 'wecare@efadcar.com';
-$config['smtp_pass']        = 'Wecare123456%';
+$config['smtp_user']        = 'test@efadcar.com';
+$config['smtp_pass']        = 'hema==123';
 $config['charset']          = 'utf-8';
 $config['mailtype']         = 'html';
 $config['priority']         = 3;
-$config['smtp_timeout']		= 1;
+$config['smtp_timeout']		= 10;
 $config['newline']="\r\n"; //"\r\n" or "\n" or "\r". DEFAULT should be "\r\n" 
 $config['crlf'] = "\r\n"; //"\r\n" or "\n" or "\r" DEFAULT should be "\r\n" 
